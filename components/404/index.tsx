5d5b893dc3838acb3b57c336e9400a0a69b73b3c
Yanggz <350926623@qq.com>
2024-05-21 11:52:37 +0800

    fix: lint

--- components/404/index.tsx
+++ components/404/index.tsx
@@ -9,26 +9,7 @@ const NotFound = memo(() => {
   const { t } = useTranslation('error');
   return (
     <div>
-      <h1
-        style={{
-          filter: 'blur(8px)',
-          fontSize: `min(${100 / 3}px, 50vw)`,
-          fontWeight: 'bolder',
-          margin: 0,
-          opacity: 0.12,
-          position: 'absolute',
-          zIndex: 0,
-        }}
-      >
-        404
-      </h1>
-      <h2 style={{ fontWeight: 'bold', marginTop: '1em', textAlign: 'center' }}>
-        {t('notFound.title')}
-      </h2>
-      <p style={{ marginBottom: '2em' }}>{t('notFound.desc')}</p>
-      <Link href="/">
-        <Button type={'primary'}>{t('notFound.backHome')}</Button>
-      </Link>
+      404
     </div>
   );
 });
