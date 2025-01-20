ad70530005f26782a1fc6cd5deb8bfef3fc49ea0
Yanggz <350926623@qq.com>
2025-01-20 11:00:31 +0800

    adyen Advanced flow支付集成

---
+++ app/_utils/constants.ts
@@ -0,0 +1,4 @@
+export const DEFAULT_LOCALE = "zh-CN";
+export const DEFAULT_COUNTRY = "US";
+export const DEFAULT_SHOPPER_REFERENCE = "jonny-jansen";
+export const DEFAULT_AMOUNT = "2000";
--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -1,12 +1,15 @@
 import { NextPage } from "next";
+import { Suspense } from "react";
+
+import AdvancedFlow from "@/app/_components/advanced-flow";
 
 const Pay: NextPage = () => {
 
   return (
-    <main>
-      Pay Center
-
-
+    <main style={{ padding: '0.32rem' }}>
+      <Suspense fallback={"loading"}>
+        <AdvancedFlow />
+      </Suspense>
     </main>
   );
 }
